e2dde6f6ee3c6854194130941fc56220c9d227ce
Leandro Martinez <lmartine@unicamp.br>
2022-11-17 10:58:13 -0300

    added tests

---
+++ tests/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd ..
+make clean
+make devel
+cd ./tests
+../bin/mdlovofit -f 0.7 -t output.pdb ./data/trajectory.pdb > rmsd.dat
+if [ ! -z `diff rmsd.dat ./results/TEST1.dat` ]; then
+    echo "ERROR in TEST1"
+    exit
+else
+    echo "TEST1 passed!"
+fi
+../bin/mdlovofit -iref 13 -f 0.7 -t output.pdb ./data/trajectory.pdb > rmsd.dat
+if [ ! -z `diff rmsd.dat ./results/TEST2.dat` ]; then
+    echo "ERROR in TEST2"
+    exit
+else
+    echo "TEST2 passed!"
+fi
+split --lines 8784 ./data/trajectory.pdb split
+../bin/mdlovofit -iref 13 -f 0.7 -t output.pdb splitaa splitab splitac splitad > rmsd.dat
+if [ ! -z `diff rmsd.dat ./results/TEST3.dat` ]; then
+    echo "ERROR in TEST3"
+    exit
+else
+    echo "TEST3 passed!"
+fi
+\rm -f rmsd.dat output.pdb split*

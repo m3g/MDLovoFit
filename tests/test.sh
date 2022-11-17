cd1316320f73e461bdf624239123d407d99b3dfc
Leandro Martinez <lmartine@unicamp.br>
2022-11-17 11:05:10 -0300

    ignore version in diff

--- tests/test.sh
+++ tests/test.sh
@@ -4,14 +4,14 @@ make clean
 make devel
 cd ./tests
 ../bin/mdlovofit -f 0.7 -t output.pdb ./data/trajectory.pdb > rmsd.dat
-if [ ! -z `diff rmsd.dat ./results/TEST1.dat` ]; then
+if [ ! -z `diff -I"Version" rmsd.dat ./results/TEST1.dat` ]; then
     echo "ERROR in TEST1"
     exit
 else
     echo "TEST1 passed!"
 fi
 ../bin/mdlovofit -iref 13 -f 0.7 -t output.pdb ./data/trajectory.pdb > rmsd.dat
-if [ ! -z `diff rmsd.dat ./results/TEST2.dat` ]; then
+if [ ! -z `diff -I"Version" rmsd.dat ./results/TEST2.dat` ]; then
     echo "ERROR in TEST2"
     exit
 else
@@ -19,7 +19,7 @@ else
 fi
 split --lines 8784 ./data/trajectory.pdb split
 ../bin/mdlovofit -iref 13 -f 0.7 -t output.pdb splitaa splitab splitac splitad > rmsd.dat
-if [ ! -z `diff rmsd.dat ./results/TEST3.dat` ]; then
+if [ ! -z `diff -I"Version" rmsd.dat ./results/TEST3.dat` ]; then
     echo "ERROR in TEST3"
     exit
 else
